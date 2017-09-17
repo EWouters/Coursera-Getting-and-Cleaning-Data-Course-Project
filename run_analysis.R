8d17c48d24e2225a60aa8bd3e5328e1b0dee95c1
EWouters <E.H.Wouters@student.tudelft.nl>
2017-09-18 01:12:28 +1200

    Load data into data.table

--- run_analysis.R
+++ run_analysis.R
@@ -1,55 +1,63 @@
 # Load data.table library
 library(data.table)
+library(dplyr)
 
 # Set name of datafolder
 datafolder <- "UCI HAR Dataset"
+ncolIS <- 128
+placeholderstr <- "~PLACEH~"
 
 # Function to read the files
 readfile <- function(...) {
-    read.csv(file.path(datafolder, ...), header = F, sep = " ")
+    fread(file.path(datafolder, ...))
 }
 
 # Read Activity labels and Features
 activity_labels <- readfile("activity_labels.txt")
 features <- readfile("features.txt")
 
-nfeatures <- length(features[[1]])
+# Read Subject test, X test and y test
+subject_test <- readfile("test", "subject_test.txt")
+X_test <- readfile("test", "X_test.txt")
+y_test <- readfile("test", "y_test.txt")
 
+# Read Subject train, X train and y train
+subject_train <- readfile("train", "subject_train.txt")
+X_train <- readfile("train", "X_train.txt")
+y_train <- readfile("train", "y_train.txt")
 
-# Function to read the datafiles
-readdf <- function(...) {
-    read.fwf(file.path(datafolder, ...), rep.int(16, nfeatures))
+# Create the data.table
+dt <- rbind(X_test, X_train)
+rm(X_test, X_train)
+
+# Add names of columns in X_test
+colnames(dt) <- unlist(features[,2])
+rm(features)
+
+# Add y and subject to data.table
+dt[,y := rbind(y_test, y_train)][,subject := rbind(subject_test, subject_train)]
+rm(y_test, y_train, subject_test, subject_train)
+
+#Function to extract the file names of the Inertial Signals folders
+getfilebases <- function(subdir) {
+    unlist(lapply(list.files(file.path(datafolder, subdir, "Inertial Signals")),
+           function(x) {sub("test|train", placeholderstr, x)}))
 }
 
-# Read Subject test, X test and Y test
-subject_test <- readfile("test", "subject_test.txt")
-X_test <- readdf("test", "X_test.txt")
-Y_test <- readdf("test", "Y_test.txt")
+# Check both Inertial Signals folders to make sure they contain the same file names
+filebase <- getfilebases("test")
+if (!setequal(filebase, getfilebases("train"))) {
+    stop("The \"Inertial Signals\" files for test and train do not match.")
+}
 
-# Read Test Inertial Signals
-body_acc_x_test <- readdf("test", "Inertial Signals", "body_acc_x_test.txt")
-body_acc_y_test <- readdf("test", "Inertial Signals", "body_acc_y_test.txt")
-body_acc_z_test <- readdf("test", "Inertial Signals", "body_acc_z_test.txt")
-body_gyro_x_test <- readdf("test", "Inertial Signals", "body_gyro_x_test.txt")
-body_gyro_y_test <- readdf("test", "Inertial Signals", "body_gyro_y_test.txt")
-body_gyro_z_test <- readdf("test", "Inertial Signals", "body_gyro_z_test.txt")
-total_acc_x_test <- readdf("test", "Inertial Signals", "total_acc_x_test.txt")
-total_acc_y_test <- readdf("test", "Inertial Signals", "total_acc_y_test.txt")
-total_acc_z_test <- readdf("test", "Inertial Signals", "total_acc_z_test.txt")
+# Function to read Inertial Signals files
+readIS <- function(subdir, thiscolname) {
+    fread(file.path(datafolder, subdir, "Inertial Signals", sub(placeholderstr, subdir, thiscolname)))
+}
 
-# Read Subject train, X train and Y train
-subject_train <- readfile("train", "subject_train.txt")
-X_train <- readdf("train", "X_train.txt")
-Y_train <- readdf("train", "Y_train.txt")
-
-# Read Train Inertial Signals
-body_acc_x_train <- readdf("train", "Inertial Signals", "body_acc_x_train.txt")
-body_acc_y_train <- readdf("train", "Inertial Signals", "body_acc_y_train.txt")
-body_acc_z_train <- readdf("train", "Inertial Signals", "body_acc_z_train.txt")
-body_gyro_x_train <- readdf("train", "Inertial Signals", "body_gyro_x_train.txt")
-body_gyro_y_train <- readdf("train", "Inertial Signals", "body_gyro_y_train.txt")
-body_gyro_z_train <- readdf("train", "Inertial Signals", "body_gyro_z_train.txt")
-total_acc_x_train <- readdf("train", "Inertial Signals", "total_acc_x_train.txt")
-total_acc_y_train <- readdf("train", "Inertial Signals", "total_acc_y_train.txt")
-total_acc_z_train <- readdf("train", "Inertial Signals", "total_acc_z_train.txt")
-
+# Loop over Inertial Signals files and add them to the data.table
+for (filename in filebase) {
+    thiscolname <- strsplit(filename, paste(".", placeholderstr, sep = ""))[[1]][1]
+    dt[,paste(rep(thiscolname, ncolIS), 1:ncolIS, sep = ".") :=
+           rbind(readIS("test", filename), readIS("train", filename))]
+}
